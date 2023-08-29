6d078705b7bf88434acfcc9752e14a8d3b9bb91e
Shujaat <shujaat@tracebloc.io>
2023-08-29 12:20:45 +0530

    added models for pytorch

--- model_zoo/pytorch/vit_16.py
+++ model_zoo/pytorch/vit_16.py
@@ -1,4 +1,5 @@
 import torch
+import torchvision
 import torch.nn as nn
 
 framework = "pytorch"
@@ -7,11 +8,12 @@ image_size = 224
 batch_size = 16
 
 
-
 class MyModel(nn.Module):
     def __init__(self):
         super(MyModel, self).__init__()
-        self.model = torch.hub.load('pytorch/vision:v0.10.0', 'vgg11', pretrained=False, num_classes=2)
+        self.model = torchvision.models.vit_b_16(
+            pretrained=False, progress=True, num_classes=3
+        )
 
     def forward(self, x):
         return self.model(x)
