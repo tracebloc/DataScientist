f967065d086807db5e3f6f9eb344983d368dabc7
Shujaat <shujaat@tracebloc.io>
2023-11-07 15:50:36 +0530

    adding two new models in pytorch

---
+++ model_zoo/pytorch/ViT_google.py
@@ -0,0 +1,24 @@
+import torch
+from torch import nn
+from transformers import ViTForImageClassification, ViTConfig
+
+
+framework = "pytorch"
+main_class = "VisionTransformer"
+image_size = 224
+batch_size = 16
+
+class VisionTransformer(nn.Module):
+    def __init__(self, num_labels=3):
+        super().__init__()
+        # Initialize the model with a specified number of output labels for classification
+        config = ViTConfig.from_pretrained('google/vit-base-patch16-224', num_labels=num_labels)
+        self.vit = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224', config=config,
+            ignore_mismatched_sizes=True)
+
+    def forward(self, pixel_values):
+        # The model will output a dictionary with various keys.
+        outputs = self.vit(pixel_values=pixel_values)
+        # The logits are now directly available from the output's 'logits' key.
+        logits = outputs.logits
+        return logits
---
+++ model_zoo/pytorch/vit.py
@@ -0,0 +1,27 @@
+import torch
+from torch import nn
+from transformers import ViTModel, ViTConfig
+
+framework = "pytorch"
+main_class = "VisionTransformer"
+image_size = 224
+batch_size = 16
+
+
+class VisionTransformer(nn.Module):
+    def __init__(self, num_labels=3):
+        super().__init__()
+        # Initialize the configuration for ViT
+        self.config = ViTConfig.from_pretrained('google/vit-base-patch16-224',
+                                                num_labels=num_labels)
+
+        # Initialize the ViT model
+        self.vit = ViTModel(self.config)
+
+        # Here you can add more layers if you want, for example a classification head
+        self.classification_head = nn.Linear(self.config.hidden_size, num_labels)
+
+    def forward(self, pixel_values):
+        outputs = self.vit(pixel_values=pixel_values)
+        logits = self.classification_head(outputs.last_hidden_state[:, 0])
+        return logits
