a43a4b3e794e977ceb8645ac52ed03f7e66982d4
Shujaat <shujaat@tracebloc.io>
2024-02-06 13:36:39 +0530

    reorganize model zoo, added working yolo v1 model e2e

--- model_zoo/classification/pytorch/ViT_google.py
+++ model_zoo/classification/pytorch/ViT_google.py
@@ -8,13 +8,17 @@ main_class = "VisionTransformer"
 image_size = 224
 batch_size = 16
 
+
 class VisionTransformer(nn.Module):
     def __init__(self, num_labels=3):
         super().__init__()
         # Initialize the model with a specified number of output labels for classification
-        config = ViTConfig.from_pretrained('google/vit-base-patch16-224', num_labels=num_labels)
-        self.vit = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224', config=config,
-            ignore_mismatched_sizes=True)
+        config = ViTConfig.from_pretrained(
+            "google/vit-base-patch16-224", num_labels=num_labels
+        )
+        self.vit = ViTForImageClassification.from_pretrained(
+            "google/vit-base-patch16-224", config=config, ignore_mismatched_sizes=True
+        )
 
     def forward(self, pixel_values):
         # The model will output a dictionary with various keys.
--- model_zoo/classification/pytorch/vit.py
+++ model_zoo/classification/pytorch/vit.py
@@ -12,8 +12,9 @@ class VisionTransformer(nn.Module):
     def __init__(self, num_labels=3):
         super().__init__()
         # Initialize the configuration for ViT
-        self.config = ViTConfig.from_pretrained('google/vit-base-patch16-224',
-                                                num_labels=num_labels)
+        self.config = ViTConfig.from_pretrained(
+            "google/vit-base-patch16-224", num_labels=num_labels
+        )
 
         # Initialize the ViT model
         self.vit = ViTModel(self.config)
