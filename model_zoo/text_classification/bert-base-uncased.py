b37921b9a212c538e07342554617be2ed1ebd3d0
shujaat81 <shujaat@tracebloc.io>
2024-06-14 12:14:55 +0530

    fixed:text classification models issue

--- model_zoo/text_classification/bert-base-uncased.py
+++ model_zoo/text_classification/bert-base-uncased.py
@@ -1,8 +1,8 @@
-from transformers import AutoModelForSequenceClassification, AutoModelForCausalLM
+from transformers import AutoModelForSequenceClassification
 from transformers import AutoConfig
 
-model_id = "bert-base-uncased"
-hf_token = "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"
+model_id = 'bert-base-uncased'
+hf_token = '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'
 framework = "pytorch"
 main_class = "MyModel"
 category = "text_classification"
@@ -10,13 +10,9 @@ model_type = ""
 batch_size = 16
 output_classes = 2
 
-
 def MyModel(num_classes=2):
 
-    return AutoModelForCausalLM.from_pretrained(
-        model_id,
-        trust_remote_code=True,
+    return AutoModelForSequenceClassification.from_pretrained(model_id,
         token=hf_token,
         num_labels=num_classes,
-        ignore_mismatched_sizes=True,
-    )
+        ignore_mismatched_sizes=True)
