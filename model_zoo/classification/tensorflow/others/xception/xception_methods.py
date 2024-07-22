3502752ef810101e5a4bdb421166b41a6d751b40
Syed Saqlain <saqlain@tracebloc.io>
2024-07-22 21:39:27 +0000

    Revert "Merge branch 'restructure' into 'master'"
    This reverts merge request !14

---
+++ model_zoo/classification/tensorflow/others/xception/xception_methods.py
@@ -0,0 +1,117 @@
+import tensorflow as tf
+from tensorflow.keras import layers, utils
+
+
+# entry flow block
+
+def entry_flow(input):
+    """
+    Entry flow is the first block of Xception architecture
+    input: input image of shape (299, 299, 3)
+    """
+
+    x = layers.Conv2D(filters=32, kernel_size=(3, 3), strides=2, padding='same')(input)
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
+    x = layers.Conv2D(filters=64, kernel_size=(3, 3), padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
+
+    shortcut = layers.Conv2D(filters=128, kernel_size=(1, 1), strides=2, padding='same')(x)
+    shortcut = layers.BatchNormalization()(shortcut)
+
+    x = layers.SeparableConv2D(filters=128, kernel_size=(3, 3), padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
+    x = layers.SeparableConv2D(filters=128, kernel_size=(3, 3), padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.MaxPooling2D(pool_size=3, strides=2, padding='same')(x)
+
+    x = layers.Add()([x, shortcut])
+
+    shortcut = layers.Conv2D(filters=256, kernel_size=(1, 1), strides=2, padding='same')(x)
+    shortcut = layers.BatchNormalization()(shortcut)
+
+    x = layers.ReLU()(x)
+    x = layers.SeparableConv2D(filters=256, kernel_size=(3, 3), padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
+    x = layers.SeparableConv2D(filters=256, kernel_size=(3, 3), padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.MaxPooling2D(pool_size=3, strides=2, padding='same')(x)
+
+    x = layers.Add()([x, shortcut])
+
+    shortcut = layers.Conv2D(filters=728, kernel_size=(1, 1), strides=2, padding='same')(x)
+    shortcut = layers.BatchNormalization()(shortcut)
+
+    x = layers.ReLU()(x)
+    x = layers.SeparableConv2D(filters=728, kernel_size=(3, 3), padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
+    x = layers.SeparableConv2D(filters=728, kernel_size=(3, 3), padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.MaxPooling2D(pool_size=3, strides=2, padding='same')(x)
+
+    output_tensor = layers.Add()([x, shortcut])
+
+    return output_tensor
+
+
+# middle flow block
+
+def middle_flow(input_tensor):
+    """
+    Middle flow of the Xception Architecture
+    input_tensor: output tensor of the the entry flow
+    Middle flow will be repeated 8 times.
+    """
+    shortcut = input_tensor
+
+    x = layers.ReLU()(input_tensor)
+    x = layers.SeparableConv2D(filters=728, kernel_size=(3, 3), padding='same')(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.ReLU()(x)
+    x = layers.SeparableConv2D(filters=728, kernel_size=(3, 3), padding='same')(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.ReLU()(x)
+    x = layers.SeparableConv2D(filters=728, kernel_size=(3, 3), padding='same')(x)
+    x = layers.BatchNormalization()(x)
+
+    x = layers.Add()([x, shortcut])
+
+    return x
+
+
+def exit_flow(input_tensor, classes):
+    """
+    Exit flow of Xception architecture
+    input_tensor: output tensor of the middle flow
+    Fully connected layers before classification layer are optional
+    """
+
+    shortcut = layers.Conv2D(filters=1024, kernel_size=(1, 1), strides=2, padding='same')(input_tensor)
+    shortcut = layers.BatchNormalization()(shortcut)
+
+    x = layers.ReLU()(input_tensor)
+    x = layers.SeparableConv2D(filters=728, kernel_size=(3, 3), padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
+    x = layers.SeparableConv2D(filters=1024, kernel_size=(3, 3), padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.MaxPooling2D(pool_size=3, strides=2, padding='same')(x)
+    x = layers.Add()([x, shortcut])
+
+    x = layers.SeparableConv2D(filters=1536, kernel_size=(3, 3), padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
+    x = layers.SeparableConv2D(filters=2048, kernel_size=(3, 3), padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
+
+    x = layers.GlobalAveragePooling2D()(x)
+    classifier = layers.Dense(classes, activation='softmax')(x)
+
+    return classifier
