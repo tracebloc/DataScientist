7cc7a38d2c19901404168bf1aca06d2ea9780bd5
shujaat81 <shujaat@tracebloc.io>
2024-09-24 13:17:05 +0530

    removing non hugging face model

--- model_zoo/text_classification/pytorch/distilbert.py
+++ model_zoo/text_classification/pytorch/distilbert.py
@@ -8,10 +8,10 @@ main_class = "MyModel"
 category = "text_classification"
 model_type = ""
 batch_size = 16
-output_classes = 2
+output_classes = 5
 
 
-def MyModel(num_classes=2):
+def MyModel(num_classes=5):
 
     return AutoModelForSequenceClassification.from_pretrained(
         model_id, token=hf_token, num_labels=num_classes, ignore_mismatched_sizes=True
