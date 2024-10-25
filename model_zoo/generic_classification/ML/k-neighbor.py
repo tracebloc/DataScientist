405f3b4900d1b8a1f583cf04fd16c4caa6987c19
shujaat81 <shujaat@tracebloc.io>
2024-10-25 18:15:47 +0530

    added ml models

---
+++ model_zoo/generic_classification/ML/k-neighbor.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+from sklearn.neighbors import KNeighborsClassifier
+
+framework = "pytorch"
+model_type = "ml"
+main_class = "MyModel"
+image_size = 69
+batch_size = 4
+output_classes = 2
+category = "generic_classification"
+num_feature_points = 69
+
+def MyModel():
+    return KNeighborsClassifier(n_neighbors=5)
